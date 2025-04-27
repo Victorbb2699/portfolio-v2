c38a5b85611fc51cff20d7181ccfb96265921be7
Victorbb2699 <victolo500@gmail.com>
2025-04-27 19:01:57 +0100

    Styles to centre content

--- src/components/ui/SlideInPanel.tsx
+++ src/components/ui/SlideInPanel.tsx
@@ -10,9 +10,9 @@ export default function SlideInPanel({ id, title, content }) {
 				className={`bg-white dark:bg-black items-center justify-center fixed top-0 right-0 h-full w-1/2 bg-white shadow-lg transform transition-transform duration-300 ${isVisible ? 'translate-x-0' : 'translate-x-full'
 					}`}
 			>
-				<div className="mb-16 items-center justify-center px-4 py-12">
+				<div className="flex flex-col items-center justify-center px-4 py-12 h-full">
 					<h2 className="text-3xl font-monospace text-lightgreen dark:text-lightgreen mb-4">{title}</h2>
-					<p className="text-sm text-black dark:text-white leading-relaxed mb-6">
+					<p className="text-l text-black dark:text-white leading-relaxed mb-6 text-center max-w-md ">
 						{content}
 					</p>
 				</div>
