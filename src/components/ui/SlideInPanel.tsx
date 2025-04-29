d37df54e3fb9c4904ae021190f7326aaee561b6e
Victorbb2699 <victolo500@gmail.com>
2025-04-29 16:55:36 +0100

    -Added project image -Fixed: SlideInPanel: content now is a children for better format text

--- src/components/ui/SlideInPanel.tsx
+++ src/components/ui/SlideInPanel.tsx
@@ -1,19 +1,25 @@
+import { string } from 'astro:schema';
 import React, { useState } from 'react';
 
-export default function SlideInPanel({ id, title, content }) {
-	const [isVisible, setIsVisible] = useState(false);
-
+export default function SlideInPanel({
+	id, title, children,
+}: {
+	id: string;
+	title: string;
+	children: React.ReactNode;
+}) {
 	return (
 		<div className='flex items-center justify-center'>
 			<div
 				id={id}
-				className={`bg-white dark:bg-black items-center justify-center fixed top-0 right-0 h-full w-1/2 bg-white shadow-lg transform transition-transform duration-300 ${isVisible ? 'translate-x-0' : 'translate-x-full'
+				className={`bg-white dark:bg-black items-center justify-center fixed top-0 right-0 h-full w-1/2 bg-white shadow-lg 
+							transform transition-transform duration-300 text-justify text-lg leading-relaxed translate-x-0 translate-x-full
 					}`}
 			>
-				<div className="flex flex-col items-center justify-center px-4 py-12 h-full">
+				<div className="flex flex-col items-center justify-center px-8 py-12 h-full text-justify">
 					<h2 className="text-3xl font-monospace text-lightgreen dark:text-lightgreen mb-4">{title}</h2>
 					<p className="text-l text-black dark:text-white leading-relaxed mb-6 text-center max-w-md ">
-						{content}
+						{children}
 					</p>
 				</div>
 			</div>
